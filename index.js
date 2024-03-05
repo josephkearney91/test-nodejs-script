2202be4dd71fd47ffda298521bfc439a7891b64b
Joe Kearney <josephkearney91@gmail.com>
2024-03-05 18:27:38 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,5 @@
+// Get the current date and time
+const now = new Date();
+
+// Print "Hello World" and the current date and time to the terminal
+console.log(`Hello World\nCurrent Date and Time: ${now}`);
